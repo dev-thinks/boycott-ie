f2947545941280e08198113097c69270f776885a
dev-thinks <kaykay@programmer.net>
2020-08-18 10:13:38 -0400

    commit.

--- boycott-ie.js
+++ boycott-ie.js
@@ -1,7 +1,2 @@
 if (/*@cc_on!@*/false || (!!window.MSInputMethodContext && !!document.documentMode)){
-var lang = navigator.language||navigator.userLanguage;
-langtwo = lang.substr(0, 2);
-
-if(langtwo == 'fr'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/fr/please-upgrade-your-browser.html";}
-else if(langtwo == 'es'){window.location.href="https://boycott-ie.github.io/boycott-ie/html/es/please-upgrade-your-browser.html";}
-else{window.location.href="https://boycott-ie.github.io/boycott-ie/html/en/please-upgrade-your-browser.html";}}
+window.location.href="https://boycott-ie.github.io/boycott-ie/html/en/please-upgrade-your-browser.html";}
